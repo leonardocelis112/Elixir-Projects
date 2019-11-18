aa68ea9ad32b11e46fd9632aecbda54400db76dc
Leonardo Celis <leonardocelis112@gmail.com>
2019-11-17 21:00:51 -0500

    Identicon create a struct to handle the data

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -2,6 +2,24 @@ defmodule Identicon do
   def main(input) do
     input
     |> has_input
+    |> pick_color
+    |> build_grid
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk(3)
+    |> Enum.map(&mirrow_row/1)
+  end
+
+  def mirrow_row(row) do
+    [first, second | _tail] = row
+
+    row ++ [second, first]
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
   end
 
   def has_input(input) do
